4e84d3930ef3ef8045ca8fc99b41508655ef9621
David Grammatico <david.lugdunum@gmail.com>
2023-12-23 10:32:07 +0100

    Stack Navigator

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import { Stack } from 'expo-router';
 
 export default function DayOneScreen() {
 	return (
 		<View>
+			<Stack.Screen options={{ title: 'Day 1' }} />
 			<Text>DayOneScreen</Text>
 		</View>
 	);
